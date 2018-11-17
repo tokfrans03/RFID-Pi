762f7ab49f381795cb6c5f1e599cda734f27d0e8
tokfrans03 <34898241+tokfrans03@users.noreply.github.com>
2018-11-17 15:52:17 +0100

    added files

---
+++ unlock.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import os
+
+#change language to your keybord layout, choose between : be, br, ca, ch, cs, de, dk, es, fi, fr, gb, hr, it, no, pt, ru, si, sv, tr, us
+
+os.system('cat /home/pi/MFRC522-python/unlock.duck | cat | python /home/pi/P4wnP1/duckencoder/duckencoder.py -l LANGUAGE -p | python /home/pi/P4wnP1/hidtools/transhid.py')
